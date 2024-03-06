62637563e6c1e14cf641a6889f09943beca43180
unknown <bereitartur5@gmail.com>
2024-03-06 23:18:45 +0100

    - Create Unit Tests templates for GPIO and I2C + Test Configs

--- Test/Unit/Libs/MyLibs/Low-Level/I2C/i2c_test.cpp
+++ Test/Unit/Libs/MyLibs/Low-Level/I2C/i2c_test.cpp
@@ -8,7 +8,8 @@ TEST(MyTestGroup, TestOne)
 {
     // Wypisz bzdury na ekranie
     printf("To jest mój prosty program testowy!\n");
-    printf("----------GPIO----------\n");
+    printf("-----------I2C-------------\n");
+    printf("Bzdura 1111\n");
 }
 
 int main(int argc, char** argv)
